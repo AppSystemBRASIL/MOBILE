3a7c54bb598ee50cd81f37670fa3623842a4991d
Junior Melo <juniormelo271101@icloud.com>
2022-09-09 08:11:19 -0400

    first commit

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
-const VERSION = '1.1.1';
+const VERSION = '1.1.2';
 const DOMAINS = 'com.xcarcorretora.app';
-const BUILD = 37;
+const BUILD = 38;
 
 const API_KEY_FIREBASE = 'AIzaSyAlGCcQx9S7SmvxAArZ3tvnH1ee8FwvmUY';
 const PROJECT_ID = 'appsystembrasil-seguro';
